13a4ddd3106292050f4fae494cfd2cb309bcf9bd
Adrianus Vian <admin@idjo.cc>
2024-01-02 19:51:07 +0700

    feat: add more utility for file handling and naming

--- pkg/util/file.go
+++ pkg/util/file.go
@@ -1,10 +1,15 @@
 package util
 
 import (
+	"crypto/md5"
+	"fmt"
 	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/cocatrip/fav/pkg/logger"
+	"k8s.io/apimachinery/pkg/util/rand"
 )
 
 var log = logger.GetLogger()
@@ -12,7 +17,7 @@ var log = logger.GetLogger()
 func GetFileSize(file *os.File) int {
 	fileStat, err := file.Stat()
 	if err != nil {
-		log.Errorf("cannot stat file: %v", err)
+		log.Error(err)
 	}
 
 	return int(fileStat.Size())
@@ -21,8 +26,67 @@ func GetFileSize(file *os.File) int {
 func GetFileMode(file *os.File) fs.FileMode {
 	fileStat, err := file.Stat()
 	if err != nil {
-		log.Errorf("cannot stat file: %v", err)
+		log.Error(err)
 	}
 
 	return fileStat.Mode()
+}
+
+func GetEncFileName(file *os.File) string {
+	name := fmt.Sprintf("%s.age", file.Name())
+
+	return name
+}
+
+func GetDecFileName(file *os.File) string {
+	name := strings.TrimSuffix(file.Name(), ".age")
+
+	return name
+}
+
+func GenerateSecretFileName(name string) string {
+	f, err := os.ReadFile(name)
+	if err != nil {
+		log.Error(err)
+	}
+
+	md5Name := md5.Sum(f)
+	randName := rand.String(len(name))
+
+	secretFileName := fmt.Sprintf("%x.%s", md5Name, randName)
+
+	return secretFileName
+}
+
+func GetMd5Sum(name string) string {
+	f, err := os.ReadFile(name)
+	if err != nil {
+		log.Error(err)
+	}
+
+	md5Name := md5.Sum(f)
+
+	return fmt.Sprintf("%x", md5Name)
+}
+
+func GetFilesInDirectory(dir string) ([]string, error) {
+	var files []string
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return files, nil
 }
