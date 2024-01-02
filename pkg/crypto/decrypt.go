dba0b4ad2a773ee56f7061c691727d9a421e8310
Adrianus Vian <admin@idjo.cc>
2024-01-02 19:44:28 +0700

    feat: combine ageEncrypt with encrypt and ageDecrypt with decrypt

--- pkg/crypto/decrypt.go
+++ pkg/crypto/decrypt.go
@@ -1,19 +1,125 @@
 package crypto
 
-import "os"
+import (
+	"bytes"
+	"io"
+	"os"
+	"strings"
 
-func Decrypt(secretFile string, fileName string) error {
-	identity := ageGetIdentityFromFile(secretFile)
+	"filippo.io/age"
+	"filippo.io/age/armor"
+	"github.com/cocatrip/fav/pkg/config"
+	"github.com/cocatrip/fav/pkg/util"
+	"github.com/cocatrip/fav/shamir"
+	"github.com/spf13/viper"
+)
 
-	file, err := os.Open(fileName)
+func Decrypt(name string) error {
+	// read file
+	file, err := os.Open(name)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
+	// Download from buckets
+	var config config.Config
+	if err := viper.Unmarshal(&config); err != nil {
+		return err
+	}
+
+	// Get md5sum from encrypted file
+	md5Sum := util.GetMd5Sum(name)
+
+	for _, storage := range config.Storages {
+		if err := storage.Download(md5Sum); err != nil {
+			return err
+		}
+	}
+
+	secretFiles, err := util.GetFilesInDirectory(".tmp")
+	if err != nil {
+		return err
+	}
+
+	identity, err := shamirCombine(secretFiles)
+	if err != nil {
+		return err
+	}
+
 	if err := ageDecrypt(identity, file); err != nil {
 		return err
 	}
 
+	if err := os.RemoveAll(".tmp"); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func ageDecrypt(identity *age.X25519Identity, file *os.File) error {
+	content := make([]byte, util.GetFileSize(file))
+	if _, err := file.Read(content); err != nil {
+		return err
+	}
+
+	out := &bytes.Buffer{}
+	f := strings.NewReader(string(content))
+	armorReader := armor.NewReader(f)
+
+	r, err := age.Decrypt(armorReader, identity)
+	if err != nil {
+		return err
+	}
+
+	if _, err := io.Copy(out, r); err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(util.GetDecFileName(file), out.Bytes(), util.GetFileMode(file)); err != nil {
+		return err
+	}
+
+	if err := os.Remove(file.Name()); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+/*
+shamirCombine  acts  as  a  wrapper  for  combining  files  containing  splitted
+identity into a single identity. it will loop through the  array  of  file  name
+and  then  get  the  bytes  and   combine   them   and   parse   that   identity
+*/
+func shamirCombine(fileList []string) (*age.X25519Identity, error) {
+	var sharedSecret [][]byte
+
+	for _, fileName := range fileList {
+		file, err := os.Open(fileName)
+		if err != nil {
+			return nil, err
+		}
+		defer file.Close()
+
+		fileData, err := io.ReadAll(file)
+		if err != nil {
+			return nil, err
+		}
+
+		sharedSecret = append(sharedSecret, fileData)
+	}
+
+	identityBytes, err := shamir.Combine(sharedSecret)
+	if err != nil {
+		return nil, err
+	}
+
+	identity, err := age.ParseX25519Identity(string(identityBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	return identity, nil
 }
--- pkg/crypto/encrypt.go
+++ pkg/crypto/encrypt.go
@@ -1,19 +1,131 @@
 package crypto
 
-import "os"
+import (
+	"io"
+	"os"
 
-func Encrypt(secretFile string, fileName string) error {
-	identity := ageGetIdentityFromFile(secretFile)
+	"filippo.io/age"
+	"filippo.io/age/armor"
+	"github.com/cocatrip/fav/pkg/config"
+	"github.com/cocatrip/fav/pkg/logger"
+	"github.com/cocatrip/fav/pkg/util"
+	"github.com/cocatrip/fav/shamir"
+	"github.com/spf13/viper"
+)
 
-	file, err := os.Open(fileName)
+/*
+Encrypt will generate an age  identity  file  and  split  those  identity  using
+shamir secret sharing while using the identity to encrypt  the  named  file.  If
+successful then the splitted identity will be uploaded to buckets listed in  the
+fav config file
+*/
+func Encrypt(name string) error {
+	file, err := os.Open(name)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	if err := ageEncrypt(identity, file); err != nil {
+	identity, err := ageEncrypt(file)
+	if err != nil {
 		return err
 	}
 
-  return err
+	var config config.Config
+	if err := viper.Unmarshal(&config); err != nil {
+		return err
+	}
+
+	secretFiles, err := shamirSplit(identity, util.GetEncFileName(file), len(config.Storages))
+	if err != nil {
+		return err
+	}
+
+	for i, storage := range config.Storages {
+		if err := storage.Upload(secretFiles[i]); err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
+func ageEncrypt(file *os.File) (*age.X25519Identity, error) {
+	log := logger.GetLogger()
+
+	identity, err := age.GenerateX25519Identity()
+	if err != nil {
+		return nil, err
+	}
+
+	recipient := identity.Recipient()
+
+	log.Infof(
+		"running: age --encrypt --armor --recipient %s > %s && rm %s",
+		recipient,
+		util.GetEncFileName(file),
+		file.Name(),
+	)
+
+	encFile, err := os.Create(util.GetEncFileName(file))
+	if err != nil {
+		return nil, err
+	}
+
+	armorWriter := armor.NewWriter(encFile)
+
+	w, err := age.Encrypt(armorWriter, recipient)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := io.Copy(w, file); err != nil {
+		return nil, err
+	}
+
+	if err := w.Close(); err != nil {
+		return nil, err
+	}
+
+	if err := armorWriter.Close(); err != nil {
+		return nil, err
+	}
+
+	if err := os.Remove(file.Name()); err != nil {
+		return nil, err
+	}
+
+	return identity, nil
+}
+
+/*
+shamirSplit acts  as  a  wrapper  for  splitting  an  age  identity  into  files
+using the original file name as a suffix and  then  returns  an  array  of  file
+name that the identity was splitted
+*/
+func shamirSplit(identity *age.X25519Identity, fileName string, parts int) ([]string, error) {
+	sharedSecret, err := shamir.Split([]byte(identity.String()), parts, parts)
+	if err != nil {
+		return nil, err
+	}
+
+	secretFiles := make([]string, 0)
+
+	for _, secret := range sharedSecret {
+		secretFileName := util.GenerateSecretFileName(fileName)
+
+		secretFile, err := os.Create(secretFileName)
+		if err != nil {
+			return nil, err
+		}
+		defer secretFile.Close()
+
+		if _, err := secretFile.Write(secret); err != nil {
+			return nil, err
+		}
+
+		secretFiles = append(secretFiles, secretFileName)
+	}
+
+	return secretFiles, nil
 }
